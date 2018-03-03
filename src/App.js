eea7d483232b0d982d41a69c8c77b3a2b84a8220
Piotr Szablewski <piotr.szablewski@vp.pl>
2018-03-03 08:20:37 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Grid } from "react-bootstrap";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Home from "./scenes/Home";
+import Collection from "./scenes/Collection";
+import Photo from "./scenes/Photo";
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Grid>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/collections/:colId" component={Collection} />
+            <Route path="/photos/:photoId" component={Photo} />
+          </Switch>
+        </Grid>
+      </BrowserRouter>
+    );
